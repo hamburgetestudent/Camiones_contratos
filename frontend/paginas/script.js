bfbe3d69001b8c396a17d6b77c30c490c569a069
Gabo292-UV <gabriel.alvarez@estudiantes.uv.cl>
2026-08-29 12:59:51 -0400

    se agrego un html, css y js para login

---
+++ frontend/paginas/script.js
@@ -0,0 +1,10 @@
+// Obtiene el botón de inicio de sesión
+const btnLogin = document.getElementById("btnLogin");
+
+// Detecta cuando se presiona el botón
+btnLogin.addEventListener("click", () => {
+
+    // Muestra un mensaje de prueba
+    alert("Botón de inicio de sesión funcionando");
+
+});
